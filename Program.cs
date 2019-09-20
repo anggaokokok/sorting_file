da05e1e03b1259bcc6af8c03ab09987147ed1e58
anggaokokok <anggaokokok@yahoo.com>
2019-09-20 16:23:23 +0700

    add lenght string

--- Program.cs
+++ Program.cs
@@ -10,21 +10,28 @@ namespace myApp
         {
             try
             {   // Open the text file using a stream reader.
-                using (StreamReader sr = new StreamReader(@"C:\Users\IT-YUSUF\Desktop\coba.txt"))
-                {
-                    // Read the stream to a string, and write the string to the console.
-                    //String line = sr.ReadToEnd();
-                    //Console.WriteLine(line);
-                    int counter = 0;
-					string ln;
+                // using (StreamReader sr = new StreamReader(@"C:\Users\IT-YUSUF\Desktop\coba.txt"))
+                // {
+                //     // Read the stream to a string, and write the string to the console.
+                //     //String line = sr.ReadToEnd();
+                //     //Console.WriteLine(line);
+                //     int counter = 0;
+				// 	string ln;
 					  
-					while ((ln = sr.ReadLine()) != null)
-					{
-						Console.WriteLine(ln);
-						counter++;
-					}
-					sr.Close();
-					Console.WriteLine($"File has {counter} lines.");
+				// 	while ((ln = sr.ReadLine()) != null)
+				// 	{
+				// 		Console.WriteLine(ln);
+				// 		counter++;
+				// 	}
+				// 	sr.Close();
+				// 	Console.WriteLine($"File has {counter} lines.");
+                // }
+                string[] lines = File.ReadAllLines(@"C:\Users\IT-YUSUF\Desktop\coba.txt");
+                char[] spearator = {' '};
+                foreach (string line in lines)
+                {
+                    String[] strlist = line.Split(spearator,  StringSplitOptions.RemoveEmptyEntries);
+                    Console.WriteLine(strlist.Length);
                 }
             }
             catch (IOException e)
