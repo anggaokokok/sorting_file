6d8dda0f7894b0e616ee16eadd327ce5b6d27d52
anggaokokok <anggaokokok@yahoo.com>
2019-09-20 15:28:42 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(@"C:\Users\IT-YUSUF\Desktop\coba.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    //String line = sr.ReadToEnd();
+                    //Console.WriteLine(line);
+                    int counter = 0;
+					string ln;
+					  
+					while ((ln = sr.ReadLine()) != null)
+					{
+						Console.WriteLine(ln);
+						counter++;
+					}
+					sr.Close();
+					Console.WriteLine($"File has {counter} lines.");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
