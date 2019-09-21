5d628541cee3c6f20d9d8ea6dcf4e97178de6056
DESKTOP-CJNL3SA\YBA <yusufokokok@gmail.com>
2019-09-21 14:39:18 +0700

    fixing sorting method

--- Program.cs
+++ Program.cs
@@ -4,41 +4,100 @@ using System.Collections;
 
 namespace myApp
 {
+    //class untuk sorting data nama terakhir
+    //class for sorting last name data
+    class Person : IComparable
+    {
+        string full_name;
+        string last_name;
+
+        public Person(string full_name, string last_name)
+        {
+            this.full_name = full_name;
+            this.last_name = last_name;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Person other = (Person)obj;
+
+            if (String.Compare(last_name, other.last_name) < 0)
+                return -1;
+
+            return 1;
+        }
+
+        public override string ToString()
+        {
+            return full_name;
+        }        
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            try
-            {   // Open the text file using a stream reader.
-                // using (StreamReader sr = new StreamReader(@"C:\Users\IT-YUSUF\Desktop\coba.txt"))
-                // {
-                //     // Read the stream to a string, and write the string to the console.
-                //     //String line = sr.ReadToEnd();
-                //     //Console.WriteLine(line);
-                //     int counter = 0;
-				// 	string ln;
-					  
-				// 	while ((ln = sr.ReadLine()) != null)
-				// 	{
-				// 		Console.WriteLine(ln);
-				// 		counter++;
-				// 	}
-				// 	sr.Close();
-				// 	Console.WriteLine($"File has {counter} lines.");
-                // }
-                string[] lines = File.ReadAllLines(@"C:\Users\IT-YUSUF\Desktop\coba.txt");
-                char[] spearator = {' '};
-                foreach (string line in lines)
+            //mencari panjang baris pada file.txt
+            //looking for length line in file.txt
+            var lineCount = 0;
+            using (var reader = File.OpenText(@".\unsorted-names-list.txt"))
+            {
+                while (reader.ReadLine() != null)
                 {
-                    String[] strlist = line.Split(spearator,  StringSplitOptions.RemoveEmptyEntries);
-                    Console.WriteLine(strlist.Length);
+                    lineCount++;
                 }
             }
-            catch (IOException e)
+            Person[] person_temp = new Person[lineCount];
+
+            try
             {
-                Console.WriteLine("The file could not be read:");
+                StreamReader sr = new StreamReader(@".\unsorted-names-list.txt");
+
+                int count = 0;
+
+                while (!sr.EndOfStream)
+                {
+                    string data = sr.ReadLine();
+                    string full_name_temp = "";
+                    string last_name_temp = "";
+                    Console.WriteLine();
+                    string[] info = data.Split(' ');
+                    if (info.Length == 2)
+                    {
+                        full_name_temp = data;
+                        last_name_temp = info[1];
+                    }
+                    else if (info.Length == 1)
+                    {
+                        full_name_temp = data;
+                        last_name_temp = info[0];
+                    }
+                    else if (info.Length >= 3)
+                    {
+                        full_name_temp = data;
+                        last_name_temp = info[2];
+                    }
+                    person_temp[count] = new Person(full_name_temp, last_name_temp);
+
+                    count++;
+                }
+                Array.Sort(person_temp);
+                sr.Close();
+            }
+            catch(FileNotFoundException e)
+            {
                 Console.WriteLine(e.Message);
             }
+
+            File.Delete(@".\unsorted-names-list.txt");
+            StreamWriter sw = new StreamWriter(@".\sorted-names-list.txt");
+
+            Console.WriteLine();
+            foreach (Person p in person_temp)
+            {
+                Console.WriteLine(p);
+                sw.WriteLine(p);
+            }
+            sw.Close();
         }
     }
 }
